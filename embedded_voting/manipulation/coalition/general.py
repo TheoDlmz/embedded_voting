c9076de9607d307932e72df97c5695303385bdd5
Théo Delemazure <34279251+TheoDlmz@users.noreply.github.com>
2021-03-17 12:10:12 +0100

    Test & Doc (manipulation coalition)

--- embedded_voting/manipulation/coalition/general.py
+++ embedded_voting/manipulation/coalition/general.py
@@ -1,14 +1,64 @@
 
 import numpy as np
 from embedded_voting.utils.cached import DeleteCacheMixin, cached_property
+from embedded_voting.profile.ParametricProfile import ParametricProfile
+from embedded_voting.scoring.singlewinner.svd import SVDNash
 
 
 class ManipulationCoalition(DeleteCacheMixin):
-    def __init__(self, profile=None):
-        self.profile_ = profile
+    """
+    This general class is used for the analysis of the manipulability of the rule by a coalition of voter.
+    It only look if there is a trivial manipulation by a coalition of voter. That means, for some candidate c
+    different than the current winner w, gather every voter who prefers c to w, and ask them to put c first and
+    w last. If c is the new winner, then the profile can be manipulated.
 
-    def __call__(self, r):
-        self.rule_ = r
+    Parameters
+    ----------
+    profile : Profile
+        The profile of voter on which we do the analysis
+    rule : ScoringRule
+        The rule we are analysing
+
+    Attributes
+    ----------
+    profile_ : Profile
+        The profile of voter on which we do the analysis
+    rule_ : ScoringRule
+        The rule we are analysing
+    winner_ : int
+        The index of the winner of the election without manipulation
+    scores_ : float list
+        The scores of the candidates without manipulation
+    welfare_ : float list
+        The welfare of the candidates without manipulation
+
+    Examples
+    --------
+    >>> np.random.seed(42)
+    >>> scores = [[1, .2, 0], [.5, .6, .9], [.1, .8, .3]]
+    >>> my_profile = ParametricProfile(3, 3, 10, scores).set_parameters(0.8, 0.8)
+    >>> manipulation = ManipulationCoalition(my_profile, SVDNash())
+    >>> manipulation.winner_
+    1
+    >>> manipulation.welfare_
+    [0.700152659355562, 1.0, 0.0]
+
+    """
+    def __init__(self, profile, rule=None):
+        self.profile_ = profile
+        self.rule_ = rule
+        if rule is not None:
+            global_rule = self.rule_(self.profile_)
+            self.winner_ = global_rule.winner_
+            self.scores_ = global_rule.scores_
+            self.welfare_ = global_rule.welfare_
+        else:
+            self.winner_ = None
+            self.scores_ = None
+            self.welfare_ = None
+
+    def __call__(self, rule):
+        self.rule_ = rule
         global_rule = self.rule_(self.profile_)
         self.winner_ = global_rule.winner_
         self.scores_ = global_rule.scores_
@@ -16,44 +66,108 @@ class ManipulationCoalition(DeleteCacheMixin):
         self.delete_cache()
         return self
 
-    def manipulation_triviale(self, cand, verbose=False):
+    def trivial_manipulation(self, candidate, verbose=False):
+        """
+        This function compute if a trivial manipulation is possible for the candidate
+        passed as parameter.
+
+        Parameters
+        ----------
+        candidate : int
+            The index of the candidate for which we manipulate.
+        verbose : bool
+            Verbose mode. By default, is set to False.
+
+        Return
+        ------
+        bool
+            If True, the profile is manipulable for this candidate.
+
+        Examples
+        --------
+        >>> np.random.seed(42)
+        >>> scores = [[1, .2, 0], [.5, .6, .9], [.1, .8, .3]]
+        >>> my_profile = ParametricProfile(3, 3, 10, scores).set_parameters(0.8, 0.8)
+        >>> manipulation = ManipulationCoalition(my_profile, SVDNash())
+        >>> manipulation.trivial_manipulation(0, verbose=True)
+        3 voters interested to elect 0 instead of 1
+        Winner is 0
+        True
+        """
 
         voters_interested = []
-        for i in range(self.profile_.n):
+        for i in range(self.profile_.n_voters):
             score_i = self.profile_.scores[i]
-            if score_i[self.winner_] < score_i[cand]:
+            if score_i[self.winner_] < score_i[candidate]:
                 voters_interested.append(i)
 
         if verbose:
-            print("%i voters interested to elect %i instead of %i"%(len(voters_interested), cand, self.winner_))
+            print("%i voters interested to elect %i instead of %i" %
+                  (len(voters_interested), candidate, self.winner_))
 
         old_profile = self.profile_.scores.copy()
         for i in voters_interested:
-            self.profile_.scores[i] = np.zeros(self.profile_.m)
-            self.profile_.scores[i][cand] = 1
+            self.profile_.scores[i] = np.zeros(self.profile_.n_candidates)
+            self.profile_.scores[i][candidate] = 1
 
         new_winner = self.rule_(self.profile_).winner_
         self.profile_.scores = old_profile
 
         if verbose:
-            print("Winner is %i"%new_winner)
-        return (new_winner == cand)
+            print("Winner is %i" % new_winner)
+
+        return new_winner == candidate
 
     @cached_property
     def is_manipulable_(self):
-        for i in range(self.profile_.m):
+        """
+        A function that quickly compute if the profile is manipulable
+
+        Return
+        ------
+        bool
+            If True, the profile is manipulable for some candidate.
+
+        Examples
+        --------
+        >>> np.random.seed(42)
+        >>> scores = [[1, .2, 0], [.5, .6, .9], [.1, .8, .3]]
+        >>> my_profile = ParametricProfile(3, 3, 10, scores).set_parameters(0.8, 0.8)
+        >>> manipulation = ManipulationCoalition(my_profile, SVDNash())
+        >>> manipulation.is_manipulable_
+        True
+        """
+
+        for i in range(self.profile_.n_candidates):
             if i == self.winner_:
                 continue
-            if self.manipulation_triviale(i):
+            if self.trivial_manipulation(i):
                 return True
         return False
 
     @cached_property
     def worst_welfare_(self):
-        welf = self.welfare_[self.winner_]
-        for i in range(self.profile_.m):
+        """
+        A function that compute the worst welfare attainable by coalition manipulation.
+
+        Return
+        ------
+        float
+            The worst welfare.
+
+        Examples
+        --------
+        >>> np.random.seed(42)
+        >>> scores = [[1, .2, 0], [.5, .6, .9], [.1, .8, .3]]
+        >>> my_profile = ParametricProfile(3, 3, 10, scores).set_parameters(0.8, 0.8)
+        >>> manipulation = ManipulationCoalition(my_profile, SVDNash())
+        >>> manipulation.worst_welfare_
+        0.0
+        """
+        worst_welfare = self.welfare_[self.winner_]
+        for i in range(self.profile_.n_candidates):
             if i == self.winner_:
                 continue
-            if self.manipulation_triviale(i):
-                welf = min(welf, self.welfare_[i])
-        return welf
+            if self.trivial_manipulation(i):
+                worst_welfare = min(worst_welfare, self.welfare_[i])
+        return worst_welfare
--- embedded_voting/manipulation/coalition/ordinal.py
+++ embedded_voting/manipulation/coalition/ordinal.py
@@ -1,37 +1,91 @@
 
+import numpy as np
 from embedded_voting.manipulation.coalition.general import ManipulationCoalition
+from embedded_voting.profile.ParametricProfile import ParametricProfile
+from embedded_voting.scoring.singlewinner.svd import SVDNash
+from embedded_voting.scoring.singlewinner.ordinal import InstantRunoffExtension
 
 
 class ManipulationCoalitionExtension(ManipulationCoalition):
+    """
+    This class extends the ManipulationCoalition class to ordinal extension (irv, borda, plurality, etc.), because
+    the ManipulationCoalition cannot be used for ordinal preferences.
 
-    def __init__(self, profile=None, extension=None):
-        self.profile_ = profile
+    Parameters
+    ----------
+    profile : Profile
+        The profile of voter on which we do the analysis.
+    extension : PositionalRuleExtension
+        The extension used.
+    rule : ScoringRule
+        The rule we are analysing.
+
+    Attributes
+    ----------
+    rule_ : ScoringRule
+        The rule we are analysing
+    extended_rule : ScoringRule
+        The rule we are analysing
+    extension : PositionalRuleExtension
+        The extension used.
+    winner_ : int
+        The index of the winner of the election without manipulation
+    welfare_ : float list
+        The welfare of the candidates without manipulation
+
+    Examples
+    --------
+    >>> np.random.seed(42)
+    >>> scores = [[1, .2, 0], [.5, .6, .9], [.1, .8, .3]]
+    >>> my_profile = ParametricProfile(3, 3, 10, scores).set_parameters(0.8, 0.8)
+    >>> extension = InstantRunoffExtension(my_profile)
+    >>> manipulation = ManipulationCoalitionExtension(my_profile, extension, SVDNash())
+    >>> manipulation.winner_
+    1
+    >>> manipulation.is_manipulable_
+    False
+    >>> manipulation.worst_welfare_
+    1.0
+    """
+
+    def __init__(self, profile, extension=None, rule=None):
+        super().__init__(profile)
         self.extension = extension
+        self.rule_ = rule
+        if rule is not None:
+            self.extended_rule = self.extension.set_rule(rule)
+            self.extended_rule(self.profile_)
+            self.winner_ = self.extended_rule.winner_
+            self.welfare_ = self.rule_(self.profile_).welfare_
+            self.delete_cache()
+        else:
+            self.extended_rule = None
 
-    def __call__(self, r):
-        self.rule_ = r
-        self.extended_rule = self.extension.set_rule(r)
+    def __call__(self, rule):
+        self.rule_ = rule
+        self.extended_rule = self.extension.set_rule(rule)
         self.extended_rule(self.profile_)
         self.winner_ = self.extended_rule.winner_
         self.welfare_ = self.rule_(self.profile_).welfare_
         self.delete_cache()
         return self
 
-    def manipulation_triviale(self, cand, verbose=False):
+    def trivial_manipulation(self, candidate, verbose=False):
 
         voters_interested = []
-        for i in range(self.profile_.n):
+        for i in range(self.profile_.n_voters):
             score_i = self.profile_.scores[i]
-            if score_i[self.winner_] < score_i[cand]:
+            if score_i[self.winner_] < score_i[candidate]:
                 voters_interested.append(i)
 
         if verbose:
-            print("%i voters interested to elect %i instead of %i" % (len(voters_interested), cand, self.winner_))
+            print("%i voters interested to elect %i instead of %i" %
+                  (len(voters_interested), candidate, self.winner_))
 
         old_profile = self.profile_.scores.copy()
         for i in voters_interested:
             self.profile_.scores[i][self.winner_] = -1
-            self.profile_.scores[i][cand] = 2
+            self.profile_.scores[i][candidate] = 2
 
         new_winner = self.extended_rule(self.profile_).winner_
         self.profile_.scores = old_profile
@@ -39,4 +93,4 @@ class ManipulationCoalitionExtension(ManipulationCoalition):
         if verbose:
             print("Winner is %i" % new_winner)
 
-        return (new_winner == cand)
+        return new_winner == candidate
