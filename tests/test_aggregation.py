e54dd85b66bf8bc2c502f67961947c3a0ae9b0d9
Théo Delemazure <34279251+TheoDlmz@users.noreply.github.com>
2021-03-25 11:55:05 +0100

    Algorithm aggregation (Test & Doc)

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -1,5 +1,7 @@
-from embedded_voting.algorithm_aggregation.score_generator import GroupedMeanGenerator
+from embedded_voting.algorithm_aggregation.score_generator import GroupedMeanGenerator, MultivariateGenerator
+from embedded_voting.algorithm_aggregation.auto_embeddings import AutoProfile
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def test_plot():
@@ -7,3 +9,13 @@ def test_plot():
     generator = GroupedMeanGenerator([2, 2, 2], 5, .5)
     generator.plot_scores(show=False)
     plt.close()
+    generator = MultivariateGenerator(np.ones((5, 5)), .5)
+    generator.plot_scores(show=False)
+    plt.close()
+
+
+def test_auto_profile():
+    np.random.seed(42)
+    profile = AutoProfile(20, 4)
+    profile.add_voters_auto(np.random.rand(10, 20), np.random.rand(10, 100))
+    profile.add_voters_cov(np.random.rand(10, 20), np.random.rand(10, 100), normalize_score=True)
