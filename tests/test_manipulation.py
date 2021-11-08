dd906656b96bb118e4946635f728a2692f9d2e7a
Théo Delemazure <34279251+TheoDlmz@users.noreply.github.com>
2021-11-08 17:14:18 +0100

    test

--- tests/test_manipulation.py
+++ tests/test_manipulation.py
@@ -3,9 +3,12 @@ from embedded_voting.embeddings.generator import ParametrizedEmbeddings
 from embedded_voting.profile.generator import CorrelatedRatings
 from embedded_voting.manipulation.coalition.general import ManipulationCoalition
 from embedded_voting.scoring.singlewinner.svd import SVDNash
+import matplotlib.pyplot as plt
 
 
 def test_coalition_general():
+    plt.clf()
+    plt.close("all")
     np.random.seed(42)
     scores_matrix = [[1, .2, 0], [.5, .6, .9], [.1, .8, .3]]
     embeddings = ParametrizedEmbeddings(10, 3)(.8)
