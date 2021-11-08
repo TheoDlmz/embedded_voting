0ab426ad262335342bfb125cb1c0cb3426dc3cd7
Théo Delemazure <34279251+TheoDlmz@users.noreply.github.com>
2021-11-08 17:42:37 +0100

    test

--- embedded_voting/profile/__init__.py
+++ embedded_voting/profile/__init__.py
@@ -2,3 +2,4 @@
 from embedded_voting.profile.impartial import *
 from embedded_voting.profile.ratings import *
 from embedded_voting.profile.generator import *
+from embedded_voting.profile.moving import *
--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -1,5 +1,6 @@
 from embedded_voting.scoring.singlewinner.features import FeaturesRule
 from embedded_voting.scoring.singlewinner.svd import SVDMax
+from embedded_voting.scoring.multiwinner.svd import IterSVD
 from embedded_voting.profile.ratings import Ratings
 from embedded_voting.embeddings.embeddings import Embeddings
 import numpy as np
@@ -28,3 +29,12 @@ def test_svd():
     embeddings = Embeddings(np.array(([[1, 1, 2], [0, 1, 1]])))
     SVDMax()(ratings, embeddings)
 
+
+def test_multi():
+    ratings = Ratings(np.array([[.5, .6, .3], [.7, 0, .2], [.2, 1, .8]]))
+    embeddings = Embeddings(np.array([[1, 1, 0], [1, 0, 1], [0, 1, 0]]))
+    election = IterSVD(k=2)(ratings, embeddings)
+    election.plot_features("3D", show=False)
+    election.plot_features("ternary", show=False)
+    with pytest.raises(ValueError):
+        election.plot_features("3D", dim=[0, 1], show=False)
