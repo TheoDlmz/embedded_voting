a1c6f6e285c06853e281dbcfd25d2671caa94798
francois-durand <fradurandpub@gmail.com>
2022-02-15 14:13:59 +0100

    RuleSVD: minor editing

--- embedded_voting/rules/singlewinner_rules/rule_svd.py
+++ embedded_voting/rules/singlewinner_rules/rule_svd.py
@@ -31,19 +31,6 @@ class RuleSVD(Rule):
         If True, consider the rank of the matrix when doing the ranking.
         By default, it is False.
 
-    Attributes
-    ----------
-    aggregation_rule : callable
-        The aggregation rule for the singular values.
-        Input : float list. Output : float.
-        By default, it is the product of the singular values.
-    square_root: boolean
-        If True, use the square root of score in the matrix.
-        By default, it is True.
-    use_rank : boolean
-        If True, consider the rank of the matrix when doing the ranking.
-        By default, it is False.
-
     Examples
     --------
     >>> ratings = Ratings(np.array([[.5, .6, .3], [.7, 0, .2], [.2, 1, .8]]))
@@ -59,31 +46,29 @@ class RuleSVD(Rule):
     [1.0, 0.0, 0.16044515869439...]
 
     """
-    def __init__(self, aggregation_rule=np.prod, square_root=True, use_rank=False):
-        score_components = 1
-        if use_rank:
-            score_components = 2
-        super().__init__(score_components=score_components)
-        self.square_root = square_root
+    def __init__(self, aggregation_rule=np.prod, square_root=True, use_rank=False, embedded_from_ratings=None):
         self.aggregation_rule = aggregation_rule
+        self.square_root = square_root
         self.use_rank = use_rank
+        score_components = 2 if use_rank else 1
+        super().__init__(score_components=score_components, embeddings_from_ratings=embedded_from_ratings)
 
     def _score_(self, candidate):
-        embeddings = self.embeddings_.times_ratings_candidate(np.sqrt(self.ratings_.candidate_ratings(candidate)))
+        m_candidate = self.embeddings_.times_ratings_candidate(np.sqrt(self.ratings_.candidate_ratings(candidate)))
 
         if self.embeddings_.shape[1] == 0:
             return self.aggregation_rule(self.ratings_.candidate_ratings(candidate))
 
-        if embeddings.shape[0] < embeddings.shape[1]:
-            embeddings_matrix = embeddings.dot(embeddings.T)
+        if m_candidate.shape[0] < m_candidate.shape[1]:
+            embeddings_matrix = m_candidate.dot(m_candidate.T)
         else:
-            embeddings_matrix = embeddings.T.dot(embeddings)
+            embeddings_matrix = m_candidate.T.dot(m_candidate)
 
         s = np.linalg.eigvals(embeddings_matrix)
         s = np.maximum(s, np.zeros(len(s)))
         s = np.sqrt(s)
         if self.use_rank:
-            matrix_rank = np.linalg.matrix_rank(embeddings)
+            matrix_rank = np.linalg.matrix_rank(m_candidate)
             return matrix_rank, self.aggregation_rule(s[:matrix_rank])
         else:
             return self.aggregation_rule(s)
