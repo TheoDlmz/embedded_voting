3e3f25b08c6b8118fb78c8c495c733e9b0842afa
francois-durand <fradurandpub@gmail.com>
2022-02-15 17:22:30 +0100

    Fix bug in `RuleFast`

--- embedded_voting/rules/singlewinner_rules/rule_fast.py
+++ embedded_voting/rules/singlewinner_rules/rule_fast.py
@@ -1,7 +1,7 @@
 import numpy as np
 from embedded_voting.rules.singlewinner_rules.rule import Rule
 from embedded_voting.embeddings_from_ratings.embeddings_from_ratings_correlation import EmbeddingsFromRatingsCorrelation
-from embedded_voting.ratings.ratings import Ratings
+from embedded_voting.embeddings_from_ratings.embeddings_from_ratings_self import EmbeddingsFromRatingsSelf
 from embedded_voting.embeddings.embeddings import Embeddings
 
 
@@ -43,7 +43,7 @@ class RuleFast(Rule):
 
     """
     def __init__(self, f=None, aggregation_rule=np.prod):
-        super().__init__()
+        super().__init__(embeddings_from_ratings=EmbeddingsFromRatingsSelf(norm=False))
         self.aggregation_rule = aggregation_rule
         if f is None:
             self.f = lambda x: np.sqrt(np.maximum(0, x/np.linalg.norm(x)))
@@ -53,23 +53,13 @@ class RuleFast(Rule):
         self._modified_ratings = None
 
     def __call__(self, ratings, embeddings=None):
-        ratings = Ratings(ratings)
-        modified_ratings = np.zeros(ratings.shape)
-        for i in range(ratings.n_voters):
-            modified_ratings[i] = self.f(ratings.voter_ratings(i))
-        self.ratings_ = ratings
+        super().__call__(ratings, embeddings)
+        modified_ratings = np.zeros(self.ratings_.shape)
+        for i in range(self.ratings_.n_voters):
+            modified_ratings[i] = self.f(self.ratings_.voter_ratings(i))
         self._modified_ratings = modified_ratings
-
-        embeddings_from_ratings = EmbeddingsFromRatingsCorrelation()
-        if embeddings is None:
-            self.embeddings_ = self.ratings_
-        else:
-            self.embeddings_ = np.concatenate([embeddings, self.ratings_], axis=1)
-        self.correlations_ = embeddings_from_ratings(self.embeddings_)
-
-        self.n_v = self.correlations_.n_sing_val_  # embeddings_from_ratings.n_sing_val_
-        self.delete_cache()
-
+        self.correlations_ = EmbeddingsFromRatingsCorrelation()(self.embeddings_)
+        self.n_v = self.correlations_.n_sing_val_
         return self
 
     def _score_(self, candidate):
