22a02cc5bcfeaf332126d3d312781453d205d5c7
francois-durand <fradurandpub@gmail.com>
2022-02-18 09:05:19 +0100

    EmbeddingsFromRatingsCorrelation: clean code

--- embedded_voting/embeddings_from_ratings/embeddings_from_ratings_correlation.py
+++ embedded_voting/embeddings_from_ratings/embeddings_from_ratings_correlation.py
@@ -8,11 +8,8 @@ class EmbeddingsFromRatingsCorrelation(EmbeddingsFromRatings):
     """
     Use the correlation with each voter as the embeddings
 
-    Attributes
-    ----------
-    n_sing_val_: int
-        The number of relevant singular values when we compute the SVD. based on the Principal Component
-        Analysis (PCA)
+    The resulting embeddings have an additional attribute, `n_sing_val_`, which is the number of relevant singular
+    values when we compute the SVD. This is based on the Principal Component Analysis (PCA).
 
     Examples
     --------
@@ -28,21 +25,13 @@ class EmbeddingsFromRatingsCorrelation(EmbeddingsFromRatings):
                 [0.4472136, 0.4472136, 0.4472136, 0.4472136, 0.4472136]])
     >>> embeddings.n_sing_val_
     1
-
     """
-    def __init__(self):
-        super().__init__()
-        self.n_sing_val_ = None
-
     def __call__(self, ratings):
         ratings = Ratings(ratings)
         positions = (ratings.T / np.sqrt((ratings ** 2).sum(axis=1))).T
-        n_voters, n_candidates = ratings.shape
-        self.n_dim = n_candidates
+        n_voters, n_candidates = positions.shape
 
         u, s, v = np.linalg.svd(positions)
-
-        n_voters, n_candidates = positions.shape
         s = np.sqrt(s)
         s /= s.sum()
         n_v = 0
